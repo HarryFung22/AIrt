44c624fee4ed0012647c887a01b93430ee320dd7
harryfung22 <harryfung22@gmail.com>
2024-05-04 11:31:04 -0400

    model imported

---
+++ backend/app.py
@@ -0,0 +1,31 @@
+import os
+from dotenv import load_dotenv
+from fastapi import FastAPI, Response
+from fastapi.middleware.cors import CORSMiddleware 
+import torch
+from torch import autocast
+from diffusers import DiffusionPipeline
+from io import BytesIO
+import base64
+
+load_dotenv()
+api_key = os.environ.get('API_KEY')
+
+app = FastAPI()
+app.add_middleware(
+    CORSMiddleware,
+    allow_credentials = True,
+    allow_origins = ["*"],
+    allow_methods = ["*"],
+    allow_headers = ["*"]
+)
+
+#for model
+pipe = DiffusionPipeline.from_pretrained("stabilityai/stable-diffusion-xl-base-1.0", torch_dtype=torch.float16, use_safetensors=True, variant="fp16")
+pipe.to("cuda")
+
+@app.get("/")
+def generate(prompt: str):
+    image = pipe(prompt=prompt).images[0]
+    image.save("testimage.png")
+    return{"out": "gurt"}
